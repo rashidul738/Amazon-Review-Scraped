4640326f70ca103ec6f8fb3aadc43f877105f1c4
rashidul738 <rashidul7713@gmail.com>
2021-10-07 22:19:45 +0600

    first commit

---
+++ Amazon Review Scraped/amazon/settings.py
@@ -0,0 +1,21 @@
+
+BOT_NAME = 'amazon'
+
+SPIDER_MODULES = ['amazon.spiders']
+NEWSPIDER_MODULE = 'amazon.spiders'
+
+ROBOTSTXT_OBEY = False
+# DOWNLOAD_DELAY = 2
+# DEFAULT_REQUEST_HEADERS = {
+#     'accept': '*/*',
+#     'accept-encoding': 'gzip, deflate, br',
+#     'accept-language': 'en-GB,en;q=0.9',
+#     'origin': 'https://www.amazon.com',
+#     'referer': 'https://www.amazon.com/',
+#     'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+#     'sec-ch-ua-mobile': '?0',
+#     'sec-fetch-dest': 'empty',
+#     'sec-fetch-mode': 'cors',
+#     'sec-fetch-site': 'cross-site',
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+# }
---
+++ Amazon Review Scraped/amazon/spiders/rewiews.py
@@ -0,0 +1,67 @@
+import scrapy
+import re
+from csv import reader
+
+asin_list = []
+
+with open('asin_list.csv', 'r') as f:
+    csv_reader = reader(f)
+    for row in csv_reader:
+        asin_list.append(row[0])
+
+reviews_url = 'https://www.sosnc.gov/online_services/Search/Business_Registration_profile?Id={}'
+
+# asin_list = [
+#     '9735305',
+#     '4702686'
+#              ]
+
+
+def cleanup(input_string):
+    if input_string:
+        return re.sub(r'[//]','',input_string).strip()
+
+class RewiewsSpider(scrapy.Spider):
+    name = 'rewiews'
+
+    def start_requests(self):
+        for asin in asin_list:
+            url = reviews_url.format(asin)
+            yield scrapy.Request(url)
+
+    def parse(self, response):
+        Name = cleanup(response.xpath('//section/div/span[2]/text()').get())
+        SosId = cleanup(response.xpath('//span[text()="SosId: "]/following-sibling::span[1]/text()').get())
+        Status = cleanup(response.xpath('//span[text()="Status: "]/following-sibling::span[1]/text()').get())
+        DateFormed = cleanup(response.xpath('//section[2]/div/span[7]/text()').get())
+        Citizenship = cleanup(response.xpath('//section[2]/div/span[9]/text()').get())
+        FiscalMonth = cleanup(response.xpath('//section[2]/div/span[11]/text()').get())
+        AnnualReportDueDate = cleanup(response.xpath('//section[2]/div/span[13]/text()').get())
+        OwnerPrincipal = cleanup(response.xpath('//span/a[@itemprop="url"]/text()').get())
+        PrincipalAdress = cleanup(response.xpath('//section[3]/p/span[2]/text()').get())
+        City = cleanup(response.xpath('//section[3]/p/span[2]/text()[2]').get())
+        State = response.xpath('(//span[@style="margin-left: 3px;"])[1]/text()').get()
+        ZipCode = response.xpath('(//span[@style="margin-left: 3px;"])[2]/text()').get()
+        cl = cleanup(response.xpath('//section[5]/div/span[2]/text()').get())
+        Shares = cleanup(response.xpath('//section[5]/div/span[4]/text()').get())
+        NoParValue = cleanup(response.xpath('//section[5]/div/span[6]/text()').get())
+        url = response.url  
+
+        yield {
+            'Name': Name,
+            'SosId': SosId,
+            'Status': Status,
+            'DateFormed': DateFormed,
+            'Citizenship': Citizenship,
+            'FiscalMonth': FiscalMonth,
+            'AnnualReportDueDate': AnnualReportDueDate,
+            'OwnerPrincipal': OwnerPrincipal,
+            'PrincipalAdress': PrincipalAdress,
+            'City': City,
+            'State': State,
+            'ZipCode': ZipCode,
+            'newClasss': cl,
+            'Shares': Shares,
+            'NoParValue': NoParValue,
+            'URL': url
+            }
